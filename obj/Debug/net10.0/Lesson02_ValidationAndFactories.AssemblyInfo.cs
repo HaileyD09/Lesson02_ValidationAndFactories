9939900deca09970061add25ed392988bc805601
HaileyD09 <hailey.duan@winsor.edu>
2026-09-18 10:01:08 -0400

    asflaskjdfhksadf

--- obj/Debug/net10.0/Lesson02_ValidationAndFactories.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lesson02_ValidationAndFactories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson02_ValidationAndFactories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80a9248ccc5f1ddf20b1582ec7780dbb7ab0cc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1b6459d7e4a9d9d3f56e63125f15af91400257")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson02_ValidationAndFactories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson02_ValidationAndFactories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
